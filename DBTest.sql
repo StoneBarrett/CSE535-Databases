c9081990799c20dca643df4f4377a6b025be1ffa
Stone Barrett <90220885+StoneBarrett@users.noreply.github.com>
2022-04-30 12:27:51 -0400

    Add files via upload

---
+++ DBTest.sql
@@ -0,0 +1,6 @@
+USE CECSProject;
+INSERT INTO `HOTEL` VALUES (NULL, '456 Peepee Road', 'John Man', 20, 'pool,restaurant,gym');
+INSERT INTO `HOTEL` VALUES (NULL, '100 Poopoo Road', 'Joan Woman', 34, 'pool,restaurant,gym');
+INSERT INTO `ROOM` VALUES (45, 'regular', 2, 2, 'king', 120.33, 1);
+INSERT INTO `CUSTOMER` VALUES (NULL, 'Super Guy', '122 Road Road', 'Louisville', '11111', 'gold');
+INSERT INTO `RESERVATION` VALUES (1, 1, 45, '2022-01-01', '2022-01-03', '0000000000', '2024-03-01');
